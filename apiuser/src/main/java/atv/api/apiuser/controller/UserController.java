947d2a76c4f2b7c60c6bac74a1c9f605e248db10
Guilherme Franco <guilhermefrancodeoliveiraa@gmail.com>
2024-10-29 19:25:26 -0300

    Create Post method and send message to Kafka

--- apiuser/src/main/java/atv/api/apiuser/controller/UserController.java
+++ apiuser/src/main/java/atv/api/apiuser/controller/UserController.java
@@ -1,7 +1,11 @@
 package atv.api.apiuser.controller;
 
+import atv.api.apiuser.entity.User;
 import atv.api.apiuser.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> sendMessage(@RequestBody User user){
+        userService.sendMessage(user, "POST");
+        return ResponseEntity.ok().body(user);
+    }
 }
--- apiuser/src/main/java/atv/api/apiuser/service/UserService.java
+++ apiuser/src/main/java/atv/api/apiuser/service/UserService.java
@@ -1,7 +1,28 @@
 package atv.api.apiuser.service;
 
+import atv.api.apiuser.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    @Value("${topic.message-action-user}")
+    private String actionUser;
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+
+    public void sendMessage(User user, String action) {
+        String message =  "Name: "+ user.getName() + " Action: " + action;
+        logger.info("Message -> {}", message);
+        this.kafkaTemplate.send(actionUser, message);
+    }
 }
+
